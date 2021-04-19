a34f060a4e450b76a9e7bd0492b000199ddc74ef
Tolfx <matteus12122@gmail.com>
2021-04-19 13:51:11 +0200

    Modified config

--- src/Config.ts
+++ src/Config.ts
@@ -1,2 +1,7 @@
 export const ExpressPort = process.env.EXPRESS_PORT ?? 5623;
 export const Discord_Prefix = "%";
+export const callbackURL = "https://discord.ntikronhus.com/oauth/callback",
+    clientID = process.env.DISCORD_CLIENT_ID ?? "", 
+    clientSecret = process.env.DISCORD_CLIENT_SECRET ?? "", 
+    redirectUri = "https://discord.ntikronhus.com/"
+export const SecretAuth = process.env.SECRETAUTH ?? ""
