3b5a7ed461739c0588fab29fc18df485c89ddf9e
Tolfx <matteus12122@gmail.com>
2021-04-22 19:36:36 +0200

    Small changes

--- src/Config.ts
+++ src/Config.ts
@@ -1,5 +1,5 @@
 export const ExpressPort = process.env.EXPRESS_PORT ?? 5623;
-export const Discord_Prefix = "%";
+export const Discord_Prefix = "!";
 export const Discord_Token = process.env.DISCORD_TOKEN ?? "";
 export const callbackURL = "https://discord.ntikronhus.com/oauth/callback",
     clientID = process.env.DISCORD_CLIENT_ID ?? "", 
