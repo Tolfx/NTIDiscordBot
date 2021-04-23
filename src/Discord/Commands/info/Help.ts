d290e71fac12df6b4d0e1bdd13421a1a0cdc2c4c
Tolfx <matteus12122@gmail.com>
2021-04-23 12:11:58 +0200

    Modified help command

--- src/Discord/Commands/info/Help.ts
+++ src/Discord/Commands/info/Help.ts
@@ -8,17 +8,37 @@ export const cat = "info";
 
 export function run(client: Client, message: Message, args: string[])
 {
-    const commands = (category: string) => {
-        return client.commands.filter(e => e.cat === category)
-        .map(cmd => ` \`${cmd.name}\``);
-    } 
-    const category = client.category.map(cat => 
-            stripIndents`**${cat[0].toUpperCase() + cat.slice(1)} commands**: \n${commands(cat)}`)
-            .reduce((string, category) => string + "\n" + "\n" + category)
+    if(args[0])
+    {
+        const embed = new MessageEmbed()
+        const command = args[0];
+        const cmd = client.commands.get(command.toLowerCase());
+        let info = "";
+        if(!cmd)
+        {
+            return message.channel.send(embed.setColor("RED").setDescription(stripIndents`
+            Unable to find this command..
+            `));
+        }
 
-    console.log(category);
+        if(cmd?.name) info = `Command name: ${cmd.name} \n`;
+        if(cmd?.description) info = `Description: ${cmd.description}\n`;
+        if(cmd?.usage) info = `Usage: ${cmd.usage}`;
 
-    const Embed = new MessageEmbed();
-
-    message.channel.send(Embed.setDescription(category));
+        return message.channel.send(embed.setDescription(info));
+    }
+    else
+    {
+        const commands = (category: string) => {
+            return client.commands.filter(e => e.cat === category)
+            .map(cmd => ` \`${cmd.name}\``);
+        } 
+        const category = client.category.map(cat => 
+                stripIndents`**${cat[0].toUpperCase() + cat.slice(1)} commands**: \n${commands(cat)}`)
+                .reduce((string, category) => string + "\n" + "\n" + category)
+    
+        const Embed = new MessageEmbed();
+    
+        message.channel.send(Embed.setDescription(category));
+    }
 }
