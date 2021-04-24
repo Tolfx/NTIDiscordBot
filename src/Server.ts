6bb73141b58e5a00daa14c3811848f545197a893
Tolfx <matteus12122@gmail.com>
2021-04-24 18:27:49 +0200

    Connected to database.

--- src/Server.ts
+++ src/Server.ts
@@ -1,4 +1,12 @@
 require("dotenv").config();
 import StartDiscordBot from "./Discord/Main";
+import { MongoDB_URL } from "./Config";
+import mongoose from "mongoose";
+
+mongoose.connect(MongoDB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+});
 
 StartDiscordBot();
