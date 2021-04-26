07faea3b196ee430543867f117caf9886fee96c5
Tolfx <matteus12122@gmail.com>
2021-04-26 14:13:31 +0200

    Listing for new events

--- src/Discord/Handler/VoiceHandler.ts
+++ src/Discord/Handler/VoiceHandler.ts
@@ -1,4 +1,20 @@
 import { VoiceState } from "discord.js";
+import { Lesson } from "../../Interfaces/Lessons";
+import EventListener from "../../Lib/EventListener";
+
+//@ts-ignore
+let lessons: [Lesson] = [];
+
+EventListener.on("newLesson", (data: Lesson) => {
+    lessons.push(data)
+});
+
+EventListener.on("endedLesson", (data: Lesson) => {
+    const index = lessons.findIndex(lesson => lesson._id === data._id);
+    if (index > -1) {
+        lessons.splice(index, 1);
+    }
+});
 
 export default function VoiceHandler(
     oldState: VoiceState, 
