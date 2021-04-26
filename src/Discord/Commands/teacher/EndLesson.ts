c1d09c178dbbdaf94e676b6b7fb9e5395af17581
Tolfx <matteus12122@gmail.com>
2021-04-26 14:14:28 +0200

    Emitting ending lesson on command

--- src/Discord/Commands/teacher/EndLesson.ts
+++ src/Discord/Commands/teacher/EndLesson.ts
@@ -3,6 +3,7 @@ import Lesson from "../../../Models/Lesson";
 import ms from "ms";
 import { Student } from "../../../Interfaces/Lessons";
 import dateFormat from "date-and-time";
+import EventListener from "../../../Lib/EventListener";
 
 export const name = "end_lesson";
 
@@ -35,6 +36,7 @@ export async function run(client: Client, message: Message, args: string[])
 
     NowLesson.ended = true;
     NowLesson.save().then(() => {
+        EventListener.emit("endedLesson", NowLesson);
         return message.channel.send(`Lesson is over`);
     }).catch(e => {
         return message.channel.send(`Something went wrong while trying to end the lesson, try again later.`);
