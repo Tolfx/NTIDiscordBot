521647438b4c8fcbcbf0e6790178392d482d218c
Tolfx <matteus12122@gmail.com>
2021-04-27 20:53:07 +0200

    mmlol headache

--- src/Express/Middlewares/EnsureAuthentication.ts
+++ src/Express/Middlewares/EnsureAuthentication.ts
@@ -4,20 +4,18 @@ import OAuth2 from "../Structures/Oauth2";
 import jwt from "jsonwebtoken";
 import { JWT_Access_Token } from "../../Config";
 
-export default function EnsureAuth(
-    oauth: OAuth2,
-)
+export default function EnsureAuth()
 {
     return (req: Request, res: Response, next: NextFunction) => {
         const authHeader = req.headers['authorization']
         const token = authHeader && authHeader.split(' ')[1]
-        if (token == null) return API_Responses.API_Error(`Missing token in headers.`, 401)
+        if (token == null) return API_Responses.API_Error(`Missing token in headers.`, 401)(res);
     
         jwt.verify(token, JWT_Access_Token, (err, token) => {
-            if (err) return API_Responses.API_Error(`Unauthorized user.`, 403);
+            if (err) return API_Responses.API_Error(`Unauthorized user.`, 403)(res);
             req.discord_token = token;
             next();
-        })
+        });
     }
 
 }
--- src/Express/Routers/User.ts
+++ src/Express/Routers/User.ts
@@ -18,7 +18,7 @@ export default class UserRouter {
         this.router = Router();
         this.oauth = oauth;
         this.client = client;
-        this.server.use(EnsureAuth(this.oauth), this.router);
+        this.server.use("/user", EnsureAuth, this.router);
 
         this.router.get("/get/user-information", async (req, res) => {
             try {
