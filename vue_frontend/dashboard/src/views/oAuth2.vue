0efe66bf8d99cbb4330cf5c759ad3fcfe76ec9e0
Tolfx <matteus12122@gmail.com>
2021-04-30 23:02:46 +0200

    Modified oauth2 router on vue

--- vue_frontend/dashboard/src/views/oAuth2.vue
+++ vue_frontend/dashboard/src/views/oAuth2.vue
@@ -7,15 +7,54 @@
 </template>
 
 <script>
-import { useRouter } from 'vue-router'
+//import { useRouter } from 'vue-router';
+const jwt = require('jsonwebtoken')
 
 export default {
     setup () {
-        const router = useRouter()
+        //const router = useRouter();
 
-        //localStorage.setItem('token', 'awdawd')
-        //localStorage.removeItem('token')
-        router.push('/dashboard')
+        function getParameterByName(name, url = window.location.href) {
+            name = name.replace(/[\\[\\]]/g, '\\$&');
+            var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+                results = regex.exec(url);
+            if (!results) return null;
+            if (!results[2]) return '';
+            return decodeURIComponent(results[2].replace(/\+/g, ' '));
+        }
+        fetch("https://discord.com/api/oauth2/token", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            //@ts-ignore
+            body: new URLSearchParams({
+                "client_id": "835552682030792725",
+                // Add discord client secret
+                //@ArvidAnderson
+                "client_secret": process.env.(...),
+                "grant_type": "authorization_code",
+                "code": getParameterByName("code"),
+                "redirect_uri": "http://localhost:8080/oauth2",
+                "scope": "identify"
+            })
+        }).then(response => response.json())
+        .then(response => { 
+            const access_token = response["access_token"];
+            // Arvid fixa expiresIn grejer alltså...
+            // Får huvudvärk
+            const jwt_token = jwt.sign(
+                access_token, 
+                //@ArvidAnderson Fix this!
+                process.env.JWT_SECRET_TOKREN, 
+            );
+            localStorage.setItem('token', jwt_token);
+            console.log("Signed in");
+            //router.push('/dashboard');
+        }).catch(e => {
+            console.log(e);
+            //router.push('/');
+        })
     }
 }
 </script>
