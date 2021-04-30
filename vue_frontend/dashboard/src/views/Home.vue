3542e36ee9871d04f9cc9d31bd70e18e28712329
Tolfx <matteus12122@gmail.com>
2021-04-30 23:02:56 +0200

    Modified home in vue

--- vue_frontend/dashboard/src/views/Home.vue
+++ vue_frontend/dashboard/src/views/Home.vue
@@ -19,8 +19,26 @@ export default {
     const store = useStore()
 
     const signInWithDiscord = () => {
-      console.log('Initiating discord sign in')
+      console.log('Initiating discord sign in');
+      const callbackURL = "http://localhost:8080/oauth2",
+      clientID = "835552682030792725";
+      window.location.href = 
+      `https://discord.com/oauth2/authorize?client_id=${clientID}&redirect_uri=${encodeURIComponent(callbackURL)}&response_type=code&scope=${encodeURIComponent("identify guilds")}`;
     }
+
+    fetch("http://localhost:5623/user/get/information", {
+        method: "GET",
+        headers: {
+          "authorization": store.state.token
+        }
+      }
+    ).then(e => 
+      e.json()
+    ).then(e => console.log(e)).catch(e => {
+      console.error(e);
+    })
+
+    console.log(store.state.token);
 
     return {
       signInWithDiscord,
