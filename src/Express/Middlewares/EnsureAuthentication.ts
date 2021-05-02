549e109140ad77a9922be7cff1eda1f003c97eb2
Tolfx <matteus12122@gmail.com>
2021-05-02 11:15:22 +0200

    Modified Auth

--- src/Express/Middlewares/EnsureAuthentication.ts
+++ src/Express/Middlewares/EnsureAuthentication.ts
@@ -4,17 +4,26 @@ import OAuth2 from "../Structures/Oauth2";
 import jwt from "jsonwebtoken";
 import { JWT_Access_Token } from "../../Config";
 
-export default function EnsureAuth(req: Request, res: Response, next: NextFunction)
+export default function EnsureAuth(oauth: OAuth2)
 {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader;
-    if (token == null)
-        return API_Responses.API_Error(`Missing token in headers.`, 401)(res);
+    return (req: Request, res: Response, next: NextFunction) => {
+        const authHeader = req.headers['authorization'];
+        const token = authHeader;
+        if (token == null)
+            return API_Responses.API_Error(`Missing token in headers.`, 401)(res);
+    
+        jwt.verify(token, JWT_Access_Token, (err, token) => {
+            if (err) 
+                return API_Responses.API_Error(`Unauthorized user.`, 403)(res);
+            req.discord_token = token;
 
-    jwt.verify(token, JWT_Access_Token, (err, token) => {
-        if (err) 
-            return API_Responses.API_Error(`Unauthorized user.`, 403)(res);
-        req.discord_token = token;
-        next();
-    })
+            const User = oauth.resolveInformation(req);
+            if(!User)
+            {
+                API_Responses.API_Error("User is not authenticated", 403)(res)
+            }
+    
+            next();
+        })
+    }
 }
