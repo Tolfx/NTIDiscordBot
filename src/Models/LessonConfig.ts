b5a2ac388c7ece0df00afa3ee24c96223e59169e
Tolfx <matteus12122@gmail.com>
2021-05-02 15:40:11 +0200

    Config for lessons added :)

---
+++ src/Models/LessonConfig.ts
@@ -0,0 +1,59 @@
+import { Document, Model, model, Schema } from "mongoose"
+import IConfigLesson from "../Interfaces/ConfigLessons";
+
+const ConfigLessonSchema = new Schema
+(
+    {
+
+        name: {
+            type: String,
+            required: true,
+            unique: true
+        },
+
+        teacherId: {
+            type: String,
+            required: true,
+        },
+
+        presence: {
+            type: Object,
+            required: true,
+        },
+
+        students: {
+            type: Array,
+            required: true,
+        },
+
+        length: {
+            type: Number,
+            required: true
+        },
+
+        min_camera: {
+            type: Number,
+            required: true,
+        },
+
+        min_streaming: {
+            type: Number,
+            required: true,
+        },
+
+        min_messages: {
+            type: Number,
+            required: true,
+        },
+
+        min_mobile_time: {
+            type: Number,
+            required: false,
+        },
+
+    }
+);
+
+const ConfigLesson = model<IConfigLesson>("lessonconfig", ConfigLessonSchema);
+
+export default ConfigLesson;
