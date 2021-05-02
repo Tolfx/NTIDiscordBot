5e36d08483ec5490fe64dd2efddf6d84ebb2da77
Tolfx <matteus12122@gmail.com>
2021-05-02 15:13:26 +0200

    Modified absencetime command

--- src/Discord/Commands/teacher/AbsenceTime.ts
+++ src/Discord/Commands/teacher/AbsenceTime.ts
@@ -1,6 +1,7 @@
 import { Message, MessageEmbed, Client } from "discord.js";
 import Lesson from "../../../Models/Lesson";
 import prettyMilliseconds from "pretty-ms";
+import dateFormat from "date-and-time";
 
 export const name = "absence_time";
 
@@ -33,17 +34,22 @@ export async function run(client: Client, message: Message, args: string[])
         return;
     }
 
-
-
     const studentIndex = NowLesson.students.findIndex(e => e.memberId === user.id);
     const absence = NowLesson.students[studentIndex].absense;
+    const student = NowLesson.students[studentIndex];
     if(absence.length <= 0)
-        return message.channel.send(`User got no absence, amazing!`);
+        return message.channel.send(`<@${student.memberId}> got no absence, amazing!`);
+
+    const neverCameBack = absence.find(e => !e.cameBackAt)
+    if(neverCameBack)
+        return message.channel.send(
+            `<@${student.memberId}> left and never came back at \`${dateFormat.format(neverCameBack.leftAt, "HH:mm:ss")}\`.`
+        );
 
     const totalTime = absence.map(e => {
         //@ts-ignore
         return e.cameBackAt-e.leftAt
     }).reduce((a,c) => a+c);
 
-    message.channel.send(`User has been absence for: ${prettyMilliseconds(totalTime)}`)
+    return message.channel.send(`<@${student.memberId}> has been absence for \`${prettyMilliseconds(totalTime)}\`.`);
 }
