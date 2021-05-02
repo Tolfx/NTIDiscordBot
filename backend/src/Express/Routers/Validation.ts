3f752c3acc3d83b49e472ed0e14f733cccc0b575
Tolfx <matteus12122@gmail.com>
2021-05-02 22:00:51 +0200

    Added a validation route

---
+++ backend/src/Express/Routers/Validation.ts
@@ -0,0 +1,36 @@
+import { Router, Application } from "express";
+import OAuth2 from "../Structures/Oauth2"
+import { Client } from "discord.js";
+import API_Responses from "../Functions/ResJson";
+import jwt from "jsonwebtoken";
+import { JWT_Access_Token } from "../../Config";
+
+export default class UserRouter {
+    protected server: Application;
+    protected router: Router;
+    protected oauth: OAuth2;
+    protected client: Client;
+
+    constructor(server: Application, oauth: OAuth2, client: Client) {
+        this.server = server;
+        this.router = Router();
+        this.oauth = oauth;
+        this.client = client;
+        this.server.use("/validate", this.router);
+
+        this.router.get("/token", async (req, res) => {
+            const authHeader = req.headers['authorization'];
+            const token = authHeader;
+            if (token == null)
+                return API_Responses.API_Error(`Missing token in headers.`, 401)(res);
+        
+            jwt.verify(token, JWT_Access_Token, (err, token) => {
+                if (err) 
+                    return API_Responses.API_Error(false)(res);
+
+                API_Responses.API_Success(true)(res);
+            });
+        });
+
+    }
+}
