b60ed4b22fcc714fb7a7c1cced7536ae2c54f538
Tolfx <matteus12122@gmail.com>
2021-05-03 19:00:32 +0200

    Logging on caching.

--- backend/src/Discord/Handler/Caching.ts
+++ backend/src/Discord/Handler/Caching.ts
@@ -1,4 +1,5 @@
 import EventListener from "../../Lib/EventListener";
+import log from "../../Lib/Logger";
 import Lesson from "../../Models/Lesson";
 
 export default function reCache()
@@ -7,6 +8,7 @@ export default function reCache()
         ended: false
     }).then(e => {
         e.forEach(e => {
+            log.warning(`Caching an active lesson which issued at: ${e.issuedAt}`);
             EventListener.emit("newLesson", e)
         });
     })
