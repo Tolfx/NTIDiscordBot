c74eadc23f83353e8be0487d610e4cdbfd0b5693
Tolfx <matteus12122@gmail.com>
2021-05-08 16:29:36 +0200

    Added splash command function for start lesson

--- backend/src/Discord/Commands/teacher/StartLesson.ts
+++ backend/src/Discord/Commands/teacher/StartLesson.ts
@@ -6,7 +6,12 @@ import dateFormat from "date-and-time";
 import EventListener from "../../../Lib/EventListener";
 import StudentSchema from "../../../Models/Student";
 import log from "../../../Lib/Logger";
-import { Admin_Role_Id } from "../../../Config";
+import { arg_slash } from "../../../Interfaces/RunEvent";
+import { Interaction } from "slash-commands";
+import isAdmin from "../../../Lib/DiscordFunc/IsAdmin";
+import reply from "../../../Lib/DiscordFunc/SlashReply";
+import { TextChannel } from "discord.js";
+import { GuildMember } from "discord.js";
 
 export const name = "start_lesson";
 
@@ -14,10 +19,8 @@ export const cat = "teacher";
 
 export async function run(client: Client, message: Message, args: string[])
 {
-    //@ts-ignore
-    const isAdmin = (message?.member.roles.cache.find(e => e.id === Admin_Role_Id)) ? true : false;
     // Check if user is a techer (is admin..);
-    if(!isAdmin)
+    if(!isAdmin(message))
         return message.channel.send(`You are not an administrator`);
 
     //@ts-ignore   
@@ -97,4 +100,90 @@ export async function run(client: Client, message: Message, args: string[])
     }).save().then(e => EventListener.emit("newLesson", e));
     
     return message.channel.send(`Lesson started and ends at: \`${dateFormat.format(lessonEnds, "HH:mm:ss")}\`.`);
+}
+
+export async function run_slash(
+    client: Client,
+    interaction: Interaction,
+    author: GuildMember,
+    channel: TextChannel,
+    args: arg_slash)
+{
+    // Check if user is a techer (is admin..);
+    const sr = new reply(client, interaction);
+    if(!isAdmin(author))
+        return sr.reply(`You are not an administrator`);
+  
+    const currentVoiceChannel = author.voice.channel;
+    if(!currentVoiceChannel)
+        return sr.reply(`Please join a \`voice channel\` to start a lesson`);
+
+    //@ts-ignore
+    const time = (args?.find(e => e.name === "time"))?.value;
+
+    const amountOfTime: number = ms(time as string);
+
+    const hasAnLessonActive = await Lesson.findOne({
+        teacherId: author.id,
+        ended: false
+    });
+
+    // Tell author it already has an active lesson....
+    if(hasAnLessonActive)
+        return sr.reply(`You already have an active lesson!`);
+
+    // Gather all students
+    const students = currentVoiceChannel.members.filter((member) => member.id !== author.id);
+    const student: Array<Student> = students.map(e => {
+        
+        StudentSchema.findOne({ discordId: e.id }).then(s => {
+            if(!s)
+            {
+                new StudentSchema({
+                    discordId: e.id,
+                    schoolSoftId: "",
+                    fullname: e.nickname ?? "",
+                }).save();
+            }
+        }).catch(() => {
+            log.error(`Failed to create new user in database.`, log.trace());
+        });
+
+        return {
+            memberId: e.id,
+            //schoolSoftId?: ;
+            voiceChannelId: currentVoiceChannel.id,
+            absence: [],
+            leftVoiceChannel: false,
+            hasLeftVoiceChannel: false,
+            messages: [],
+            hasSentMessage: false,
+            cameraOn: false,
+            hasCameraBeenOn: false,
+            camereTime: [],
+            isStreaming: false,
+            hasBeenStreaming: false,
+            streamingTime: [],
+            isOnMobile: e.presence.clientStatus?.mobile ? true : false,
+            presence: e.presence.status,
+            pre_registered: false,
+        }
+    });
+
+    const mainChannel = currentVoiceChannel.id;
+
+    const date = new Date();
+    const lessonEnds = (dateFormat.addMilliseconds(date, amountOfTime));
+    
+    new Lesson({
+        teacherId: author.id,
+        mainChannel,
+        endsAt: lessonEnds,
+        ended: false,
+        break: false,
+        breaks: [],
+        students: student
+    }).save().then(e => EventListener.emit("newLesson", e));
+    
+    return sr.reply(`Lesson started and ends at: \`${dateFormat.format(lessonEnds, "HH:mm:ss")}\`.`);
 }
