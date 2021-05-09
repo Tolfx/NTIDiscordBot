6b6b3bbfeb56b76b22425ac901bc7c9268a3369f
Tolfx <matteus12122@gmail.com>
2021-05-09 17:36:10 +0200

    Modified endlesson command

--- backend/src/Discord/Commands/teacher/EndLesson.ts
+++ backend/src/Discord/Commands/teacher/EndLesson.ts
@@ -1,7 +1,10 @@
-import { Message, Client } from "discord.js";
+import { Message, Client, GuildMember, TextChannel } from "discord.js";
 import Lesson from "../../../Models/Lesson";
 import EventListener from "../../../Lib/EventListener";
 import isAdmin from "../../../Lib/DiscordFunc/IsAdmin";
+import { Interaction } from "slash-commands";
+import { arg_slash } from "../../../Interfaces/RunEvent";
+import reply from "../../../Lib/DiscordFunc/SlashReply";
 
 export const name = "end_lesson";
 
@@ -28,5 +31,36 @@ export async function run(client: Client, message: Message, args: string[])
         return message.channel.send(`Lesson is over`);
     }).catch(e => {
         return message.channel.send(`Something went wrong while trying to end the lesson, try again later.`);
+    });
+}
+
+export async function run_slash(
+    client: Client,
+    interaction: Interaction,
+    author: GuildMember,
+    channel: TextChannel,
+    args: arg_slash
+)
+{
+    const sr = (new reply(client, interaction)).reply;
+    // Check if user is a techer (is admin..);
+    if(!isAdmin(author))
+        return sr(`You are not an administrator`);
+
+    let NowLesson = await Lesson.findOne({
+        teacherId: author.id,
+        ended: false
+    });
+
+    // Tell author it already has no active lesson....
+    if(!NowLesson)
+        return sr(`You have no active lessons!`);
+
+    NowLesson.ended = true;
+    NowLesson.save().then(() => {
+        EventListener.emit("endedLesson", NowLesson);
+        return sr(`Lesson is over`);
+    }).catch(e => {
+        return sr(`Something went wrong while trying to end the lesson, try again later.`);
     });
 }
