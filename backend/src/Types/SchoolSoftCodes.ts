e9b8cba9f2d55d96f6a59693e4aea76642163d86
Tolfx <matteus12122@gmail.com>
2021-05-10 10:44:21 +0200

    Types for school soft cods

---
+++ backend/src/Types/SchoolSoftCodes.ts
@@ -0,0 +1,6 @@
+export type Absence = 1;
+export type Present = 0;
+export type Pre_registered = 9;
+export type Distance = 751;
+export type Partially_Absent = 205;
+export type Late_arrival = 206;
