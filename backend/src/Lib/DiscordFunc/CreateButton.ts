64af3c2269fc8fa957dd09f64683340bbe971000
Tolfx <matteus12122@gmail.com>
2021-06-27 13:37:33 +0200

    New function to create buttons

---
+++ backend/src/Lib/DiscordFunc/CreateButton.ts
@@ -0,0 +1,44 @@
+import { MessageActionRow, MessageButton, MessageButtonStyles } from "discord-buttons";
+import { Message, Client, GuildMember, TextChannel, MessageAdditions } from "discord.js";
+
+interface DataButton
+{
+    name: string;
+    id: string;
+    style: keyof MessageButtonStyles;
+    disabled?: Boolean;
+    emoji?: string;
+    url?: string;
+}
+
+export default function CreateButton(...data: DataButton[]): any
+{
+    let buttons = [];
+    let rows = new MessageActionRow();
+
+    for(let button of data)
+    {
+        let b = new MessageButton()
+                .setID(button.id)
+                .setLabel(button.name)
+                .setStyle(button.style)
+
+        if(button.disabled)
+            b.setDisabled(true);
+
+        if(button.emoji)
+            b.setEmoji(button.emoji);
+
+        if(button.url && button.style === "url")
+            b.setURL(button.url)
+
+        buttons.push(b);
+    }
+    
+    for(let row of buttons)
+    {
+        rows.addComponent(row)
+    }
+
+    return rows;
+}
